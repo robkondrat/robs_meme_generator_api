bc1824a5e4f0b8357cdbdc549feae862a23479af
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-04-21 16:47:43 -0500

    users routes and controller created

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,46 @@
 class Api::UsersController < ApplicationController
+  def index
+    @users = User.all
+    render "index.json.jb"
+  end
+
+  def create
+    @user = User.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name], 
+      email: params[:email], 
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
+    )
+
+    if @user.save
+      render json: { message: "User created successfully" }, status: :created
+    else
+      render json: { error: @user.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render 'show.json.jb'
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.first_name = params[:first_name] || @user.first_name
+    @user.last_name = params[:last_name] || @user.last_name
+    @user.email = params[:email] || @user.email
+
+    if @user.save
+      render "show.json.jb"
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.delete
+    render json: {message: "User ID #{user.id} successfully deleted."}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
+    get "/users" => 'users#index'
+    post '/users' => 'users#create'
+    get '/users/:id' => 'users#show'
+    patch '/users/:id' => 'users#update'
+    delete '/users/:id' => 'users#destroy'
+
     get "/memes" => 'memes#index'
     post '/memes' => 'memes#create'
     get '/memes/:id' => 'memes#show'
